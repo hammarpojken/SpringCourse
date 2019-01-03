824cce7040550702dffad83187a84cc92d3c8fe3
hammarpojken <nicke_hammar@hotmail.com>
2019-01-03 12:36:16 +0100

    Initialize a spring-boot project

---
+++ udemy-spring-level1/src/main/java/com/hammar/spring/basics/udemyspringlevel1/UdemySpringLevel1Application.java
@@ -0,0 +1,14 @@
+package com.hammar.spring.basics.udemyspringlevel1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringLevel1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringLevel1Application.class, args);
+	}
+
+}
+
---
+++ udemy-spring-level1/src/test/java/com/hammar/spring/basics/udemyspringlevel1/UdemySpringLevel1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hammar.spring.basics.udemyspringlevel1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdemySpringLevel1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
