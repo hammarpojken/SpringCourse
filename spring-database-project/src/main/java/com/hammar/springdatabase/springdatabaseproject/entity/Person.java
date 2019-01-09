87612405bc1192ae7dbaae6e0d34c15077388f89
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-09 16:26:42 +0100

    Refactor person to JPA @Entity

--- spring-database-project/src/main/java/com/hammar/springdatabase/springdatabaseproject/entity/Person.java
+++ spring-database-project/src/main/java/com/hammar/springdatabase/springdatabaseproject/entity/Person.java
@@ -2,7 +2,15 @@ package com.hammar.springdatabase.springdatabaseproject.entity;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
+	
+	@Id
+	@GeneratedValue
 	private int id;
 	private String name;
 	private String location;
@@ -14,6 +22,12 @@ public class Person {
 	public Person(int id, String name, String location, Date birthDate) {
 		super();
 		this.id = id;
+		this.name = name;
+		this.location = location;
+		this.birthDate = birthDate;
+	}
+	public Person(String name, String location, Date birthDate) {
+		super();
 		this.name = name;
 		this.location = location;
 		this.birthDate = birthDate;
