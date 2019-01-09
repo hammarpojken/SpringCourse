44886dbf04f630640098b126746341226df5db48
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-09 13:06:24 +0100

    Start spring-boot project using JDBC, JPA, H2 and Web dependencies

---
+++ spring-database-project/src/main/java/com/hammar/springdatabase/springdatabaseproject/SpringDatabaseProjectApplication.java
@@ -0,0 +1,14 @@
+package com.hammar.springdatabase.springdatabaseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatabaseProjectApplication.class, args);
+	}
+
+}
+
