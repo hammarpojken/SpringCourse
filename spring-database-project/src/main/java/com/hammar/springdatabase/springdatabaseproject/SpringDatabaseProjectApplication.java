8b7b1b511438d2f2d04b90c9a5a923cfae95eb09
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-09 15:58:24 +0100

    Add insert and update methods

--- spring-database-project/src/main/java/com/hammar/springdatabase/springdatabaseproject/SpringDatabaseProjectApplication.java
+++ spring-database-project/src/main/java/com/hammar/springdatabase/springdatabaseproject/SpringDatabaseProjectApplication.java
@@ -1,6 +1,8 @@
 package com.hammar.springdatabase.springdatabaseproject;
 
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.hammar.springdatabase.springdatabaseproject.entity.Person;
 import com.hammar.springdatabase.springdatabaseproject.jdbc.PersonJdbcDAO;
 
 @SpringBootApplication
@@ -28,6 +31,8 @@ public class SpringDatabaseProjectApplication implements CommandLineRunner {
 		logger.info(" User with id 10003 {}", dao.findById(10003));
 		logger.info(" Users with name niclas {}", dao.findByName("Niclas"));
 		logger.info(" Deleted rows {}", dao.deleteById(10005));
+		logger.info(" Inserting new person {}", dao.insert(new Person(10006, "Martin", "Trosa", new Date())));
+		logger.info(" Updated person {}", dao.update(new Person(10006,"Karl", "Trosa", new Date())));
 		
 	}
 
--- spring-database-project/src/main/java/com/hammar/springdatabase/springdatabaseproject/jdbc/PersonJdbcDAO.java
+++ spring-database-project/src/main/java/com/hammar/springdatabase/springdatabaseproject/jdbc/PersonJdbcDAO.java
@@ -1,5 +1,6 @@
 package com.hammar.springdatabase.springdatabaseproject.jdbc;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +35,26 @@ public class PersonJdbcDAO {
 		 return jdbcTemplate.update
 				("delete from person where id=?", new Object [] {id});
 	}
+	public int insert(Person person) {
+		return jdbcTemplate.update
+				("insert into person (id, name, location, birth_date) values (?,?,?,?)",
+						new Object [] {
+								person.getId(),
+								person.getName(),
+								person.getLocation(),
+								new Timestamp(person.getBirthDate().getTime())
+								});
+	}
+	public int update(Person person) {
+		return jdbcTemplate.update
+				("update person set name=?, location=?, birth_date=? where id=?",
+						new Object [] {
+								person.getName(),
+								person.getLocation(),
+								new Timestamp(person.getBirthDate().getTime()),
+								person.getId()
+				});
+	}
+
 
 }
