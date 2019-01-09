8e838ab3dfddc8201000bd185b4326650fc1e973
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-09 10:26:05 +0100

    initial commit for spring-aop-project

---
+++ spring-aop-project/src/main/java/com/hammar/springaop/springaopproject/SpringAopProjectApplication.java
@@ -0,0 +1,14 @@
+package com.hammar.springaop.springaopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopProjectApplication.class, args);
+	}
+
+}
+
---
+++ spring-aop-project/src/test/java/com/hammar/springaop/springaopproject/SpringAopProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hammar.springaop.springaopproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAopProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
