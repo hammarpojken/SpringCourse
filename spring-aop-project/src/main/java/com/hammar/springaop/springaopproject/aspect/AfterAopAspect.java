0036f575e88cad1ec868bdc5cac97ca9813f62f0
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-09 11:38:51 +0100

    Add aspect for calculation execution time on business methods with @Around

--- spring-aop-project/src/main/java/com/hammar/springaop/springaopproject/aspect/AfterAopAspect.java
+++ spring-aop-project/src/main/java/com/hammar/springaop/springaopproject/aspect/AfterAopAspect.java
@@ -1,9 +1,9 @@
 package com.hammar.springaop.springaopproject.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +14,12 @@ public class AfterAopAspect {
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	@AfterReturning(
-			value="execution(* com.hammar.springaop.springaopproject.business.*.*(..))",
-			returning="result"
-			)
-	public void afterReturning(JoinPoint joinPoint, Object result) {
+	@Around("execution(* com.hammar.springaop.springaopproject.business.*.*(..))")
+	public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+		long startTime = System.currentTimeMillis();
+		joinPoint.proceed();
+		long timeTaken = System.currentTimeMillis() - startTime;
 		
-		logger.info("{} returned with value {}", joinPoint, result);
+		logger.info(" Time taken by {} is {}", joinPoint, timeTaken);
 	}
 }
---
+++ spring-aop-project/src/main/java/com/hammar/springaop/springaopproject/aspect/MethodExecutionCalculationAspect.java
@@ -0,0 +1,25 @@
+package com.hammar.springaop.springaopproject.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class MethodExecutionCalculationAspect {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@AfterReturning(
+			value="execution(* com.hammar.springaop.springaopproject.business.*.*(..))",
+			returning="result"
+			)
+	public void afterReturning(JoinPoint joinPoint, Object result) {
+		
+		logger.info("{} returned with value {}", joinPoint, result);
+	}
+}
