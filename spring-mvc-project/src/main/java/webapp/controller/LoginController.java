ca0cef35cd071bdca0c13222ab72971e8a326892
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-10 13:54:04 +0100

    Refactor to use spring-mvc pattern

--- spring-mvc-project/src/main/java/webapp/controller/LoginController.java
+++ spring-mvc-project/src/main/java/webapp/controller/LoginController.java
@@ -1,14 +1,27 @@
 package webapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping(value= "/login")
-	public String sayHello() {
+	@RequestMapping(value= "/login", method = RequestMethod.GET)
+	public String getLogin() {
 		return "login";
+	}
+	@RequestMapping(value= "/login", method = RequestMethod.POST)
+	public String handleLoginReq(
+			@RequestParam String name,
+			@RequestParam String password,
+			ModelMap model) {
+		
+		model.put("name", name);
+		model.put("password", password);
+		return "welcome";
 	}
 }
